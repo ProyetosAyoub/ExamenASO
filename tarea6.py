fa43608f609bcffe9c420d5de8ecbae7f7885b78
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2024-02-25 23:36:54 +0100

    Update tarea6.py

--- tarea6.py
+++ tarea6.py
@@ -1,1 +1,11 @@
 
+[Unit]
+Description=Ejecutar tarea5.py cada 10 segundos
+
+[Service]
+ExecStart=/usr/bin/python3 /home/alumno/tarea5.py
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
